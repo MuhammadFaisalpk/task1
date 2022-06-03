e82e96545882763ee8e73e0e62e180d652139bbf
Muhammad Faisal <faisalriiaazz@gmail.com>
2022-06-03 10:30:30 +0500

    Initial fixings

--- app/src/main/java/com/example/task1/MainActivity.kt
+++ app/src/main/java/com/example/task1/MainActivity.kt
@@ -3,28 +3,53 @@ package com.example.task1
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
-import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentManager
-import androidx.fragment.app.FragmentStatePagerAdapter
+import androidx.fragment.app.*
 import androidx.viewpager.widget.ViewPager
-import com.example.task1.containerViews.RootTab1Fragment
-import com.example.task1.containerViews.RootTab2Fragment
-import com.example.task1.containerViews.RootTab3Fragment
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import androidx.viewpager2.widget.ViewPager2
 import com.example.task1.databinding.ActivityMainBinding
+import com.example.task1.view.DocsFragment
 import com.example.task1.view.ImagesFragment
+import com.example.task1.view.VideosFragment
 import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-    lateinit var viewPager: ViewPager
-    lateinit var tabLayout: TabLayout
+    lateinit var viewPager: ViewPager2
+    private lateinit var tabLayout: TabLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         initViews()
         setStatePageAdapter()
+        tabLayoutListener()
+
+    }
+
+    private fun initViews() {
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        viewPager = binding.pager
+        tabLayout = binding.tabs
+    }
+
+    private fun setStatePageAdapter() {
+        val adapter = AdapterTabPager(this)
+        adapter.addFragment(ImagesFragment(), "Images")
+        adapter.addFragment(VideosFragment(), "Videos")
+        adapter.addFragment(DocsFragment(), "Docs")
+
+        viewPager.adapter = adapter
+        viewPager.currentItem = 0
+        TabLayoutMediator(tabLayout, viewPager) { tab, position ->
+            tab.text = adapter.getTabTitle(position)
+        }.attach()
+    }
+
+    private fun tabLayoutListener() {
         tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
             override fun onTabSelected(tab: TabLayout.Tab) {
                 viewPager.currentItem = tab.position
@@ -50,44 +75,25 @@ class MainActivity : AppCompatActivity() {
         })
     }
 
-    private fun initViews() {
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+    class AdapterTabPager(activity: FragmentActivity?) : FragmentStateAdapter(activity!!) {
+        private val mFragmentList: MutableList<Fragment> = ArrayList()
+        private val mFragmentTitleList: MutableList<String> = ArrayList()
 
-        viewPager = binding.pager
-        tabLayout = binding.tabs
-    }
-
-    private fun setStatePageAdapter() {
-        val myViewPageStateAdapter: MyViewPageStateAdapter =
-            MyViewPageStateAdapter(supportFragmentManager)
-        myViewPageStateAdapter.addFragment(ImagesFragment(), "Images")
-        myViewPageStateAdapter.addFragment(RootTab2Fragment(), "Videos")
-        myViewPageStateAdapter.addFragment(RootTab3Fragment(), "Docs")
-        viewPager.adapter = myViewPageStateAdapter
-        tabLayout.setupWithViewPager(viewPager, true)
-
-    }
-
-    class MyViewPageStateAdapter(fm: FragmentManager) : FragmentStatePagerAdapter(fm) {
-        private val fragmentList: MutableList<Fragment> = ArrayList<Fragment>()
-        private val fragmentTitleList: MutableList<String> = ArrayList<String>()
-
-        override fun getItem(position: Int): Fragment {
-            return fragmentList[position]
-        }
-
-        override fun getCount(): Int {
-            return fragmentList.size
-        }
-
-        override fun getPageTitle(position: Int): CharSequence? {
-            return fragmentTitleList[position]
+        public fun getTabTitle(position: Int): String {
+            return mFragmentTitleList[position]
         }
 
         fun addFragment(fragment: Fragment, title: String) {
-            fragmentList.add(fragment)
-            fragmentTitleList.add(title)
+            mFragmentList.add(fragment)
+            mFragmentTitleList.add(title)
+        }
 
+        override fun getItemCount(): Int {
+            return mFragmentList.size
+        }
+
+        override fun createFragment(position: Int): Fragment {
+            return mFragmentList[position]
         }
     }
 }
--- app/src/main/java/com/example/task1/view/DocsFragment.kt
+++ app/src/main/java/com/example/task1/view/DocsFragment.kt
@@ -5,61 +5,51 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.task1.R
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [RegistrationFragment.newInstance] factory method to
- * create an instance of this fragment.
- *
- */
 class DocsFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-    lateinit var registrationView: View
+    lateinit var listFragmentView: View
 
+    //    lateinit var userViewModel: UserListViewModel
+    lateinit var recyclerView: RecyclerView
+
+    //    lateinit var userAdapter: UserListAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+
+//        userViewModel = ViewModelProviders.of(activity!!).get(UserListViewModel::class.java)
     }
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        registrationView = inflater.inflate(R.layout.fragment_registration, container, false)
-        return registrationView
+        listFragmentView = inflater.inflate(R.layout.fragment_docs, container, false)
+        initVars(listFragmentView)
+        recyclerView.layoutManager = LinearLayoutManager(activity, RecyclerView.VERTICAL, false)
+
+//        userAdapter = UserListAdapter()
+//        recyclerView.setAdapter(userAdapter)
+//        userViewModel.getAllUsers().observe(this, object : Observer<List<User>> {
+//            override fun onChanged(users: List<User>?) {
+//                // Update the cached copy of the words in the adapter.
+//                userAdapter.setListItems(users)
+//            }
+//        })
+        return listFragmentView
     }
 
+    private fun setAdapter() {
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment RegistrationFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            DocsFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    }
+
+    private fun initVars(view: View) {
+        recyclerView = view.findViewById(R.id.docs_list)
     }
 }
